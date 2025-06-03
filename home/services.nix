867de5ae4f6a2a5d6002947dccdbe10b149d1d82
Julius Kreutz <julius@kreutz.dev>
2025-06-04 00:00:02 +0200

    saving:

--- home/services.nix
+++ home/services.nix
@@ -6,6 +6,7 @@
     enableZshIntegration = true;
     defaultCacheTtl = 3600;
     pinentry.package = pkgs.pinentry-gnome3;
+    sshKeys = [ "B22F697B459E84F8146F1809917143A596F984D8" ];
   };
   services.flameshot = {
     enable = true;
