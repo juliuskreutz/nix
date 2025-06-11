291007a51a42bd55b577649b15f5b0055a5aeeb6
Julius Kreutz <julius@kreutz.dev>
2025-06-11 19:24:12 +0200

    fix: new graphics card

--- configuration/graphics/desktop.nix
+++ configuration/graphics/desktop.nix
@@ -7,16 +7,13 @@
     enable = true;
   };
 
-  environment.variables = {
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  };
-
   services.xserver.videoDrivers = [ "nvidia" ];
+
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
-    open = false;
+    open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
