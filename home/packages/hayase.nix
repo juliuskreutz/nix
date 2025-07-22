937e4005d11dc5ecc3f53b91bbca3e2391cdbe35
Julius Kreutz <julius@kreutz.dev>
2025-07-22 20:11:01 +0200

    feat: add hayase meta

--- home/packages/hayase.nix
+++ home/packages/hayase.nix
@@ -1,7 +1,7 @@
 {
+  lib,
   appimageTools,
   fetchurl,
-  makeWrapper,
 }:
 appimageTools.wrapType2 rec {
   pname = "hayase";
@@ -14,8 +14,6 @@ appimageTools.wrapType2 rec {
 
   extracted = appimageTools.extractType2 { inherit pname version src; };
 
-  nativeBuildInputs = [ makeWrapper ];
-
   extraInstallCommands = ''
     mkdir -p $out/share/applications
     mkdir -p $out/share/lib/${pname}
@@ -27,4 +25,14 @@ appimageTools.wrapType2 rec {
     substituteInPlace $out/share/applications/${pname}.desktop \
       --replace-fail 'Exec=AppRun' 'Exec=${pname}'
   '';
+
+  meta = {
+    description = "Stream anime torrents instantly, real-time with no waiting for downloads to finish! 🍿";
+    homepage = "https://hayase.watch/";
+    license = lib.licenses.bsl11;
+    maintainers = [ lib.maintainers.juliuskreutz ];
+    platforms = [ "x86_64-linux" ];
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+    mainProgram = "hayase";
+  };
 }
